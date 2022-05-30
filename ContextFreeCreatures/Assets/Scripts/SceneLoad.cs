74f58f623d7f8d7b0355de4ce0fff84543a95817
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-05-30 12:19:41 +0100

    Add transition to level 1

--- ContextFreeCreatures/Assets/Scripts/SceneLoad.cs
+++ ContextFreeCreatures/Assets/Scripts/SceneLoad.cs
@@ -16,6 +16,11 @@ public class SceneLoad : MonoBehaviour
         SceneManager.LoadScene("LevelSelection");
     }
 
+    public void ToLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
     // Update is called once per frame
     void Update()
     {
