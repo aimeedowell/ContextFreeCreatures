e5d896ae680e01b63589e7dfb64d0b25621ac9f4
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-06-10 12:48:47 +0100

    Fail when rubies do not match

--- ContextFreeCreatures/Assets/Scripts/EndWord.cs
+++ ContextFreeCreatures/Assets/Scripts/EndWord.cs
@@ -51,24 +51,26 @@ public class EndWord : MonoBehaviour
     {
         for (int i = 0; i < endWord.Count; i++)
         {
-            endWord[i].GetComponent<Animator>().enabled = true;
-
             if (i < targetWord.Count)
             {
-                targetWord[i].GetComponent<Animator>().enabled = true;
-
                 if (endWord[i].gameObject.name.Contains("Green") && targetWord[i].gameObject.name.Contains("Green"))
                 {
+                    endWord[i].GetComponent<Animator>().enabled = true;
+                    targetWord[i].GetComponent<Animator>().enabled = true;
                     endWord[i].GetComponent<Animator>().Play("GreenGemPop");
                     targetWord[i].GetComponent<Animator>().Play("TGreenPop");
                 }
                 else if (endWord[i].gameObject.name.Contains("Blue") && targetWord[i].gameObject.name.Contains("Blue"))
                 {
+                    endWord[i].GetComponent<Animator>().enabled = true;
+                    targetWord[i].GetComponent<Animator>().enabled = true;
                     endWord[i].GetComponent<Animator>().Play("BlueGemPop");
                     targetWord[i].GetComponent<Animator>().Play("TBluePop");
                 }
                 else if (endWord[i].gameObject.name.Contains("Purple") && targetWord[i].gameObject.name.Contains("Purple"))
                 {
+                    endWord[i].GetComponent<Animator>().enabled = true;
+                    targetWord[i].GetComponent<Animator>().enabled = true;
                     endWord[i].GetComponent<Animator>().Play("PurpleGemPop");
                     targetWord[i].GetComponent<Animator>().Play("TPurplePop");
                 }
