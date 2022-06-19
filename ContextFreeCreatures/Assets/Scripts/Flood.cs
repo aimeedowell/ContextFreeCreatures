4d0ccfbca32d7d1327fc5e3efc623823d055b8e1
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-06-19 19:31:31 +0100

    add timer level

---
+++ ContextFreeCreatures/Assets/Scripts/Flood.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flood : MonoBehaviour
+{
+    public GameObject startNode;
+
+    public GameObject maskContainer;
+    public GameObject mask;
+    public Text timerText;
+    GameObject cam;
+    GameObject treeArea;
+
+    bool timerStarted = false;
+
+    float amountOfTime = 35f;
+
+    float timeRemaining = 35f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        treeArea = GameObject.Find("TreeSpace");
+        cam = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+        if (timerStarted)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerStarted = false;
+                cam.GetComponent<LevelEnd>().LevelFailed();
+                Debug.Log("Time has run out!");
+            }
+            DisplayTime(timeRemaining);
+        }
+
+    }
+
+    // Update is called once per frame
+    public void OnStartClicked()
+    {
+        timerStarted = true;
+        maskContainer.SetActive(true);
+        mask.SetActive(true);
+        float lengthOfMask = treeArea.GetComponent<RectTransform>().rect.height;
+        Vector2 endSize = new Vector2(mask.GetComponent<RectTransform>().sizeDelta.x, lengthOfMask);
+
+        float startY = startNode.transform.localPosition.y;
+        float startX = startNode.transform.localPosition.x;
+        Vector2 startAnch = new Vector2(startX, startY);
+        Vector2 endAnch = new Vector2(startX, startY - (lengthOfMask/2));
+        // Vector3 endSize = 650;
+        mask.GetComponent<RectTransform>().anchoredPosition = startAnch;
+        mask.SetActive(true);
+        // maskContain.GetComponent<Animator>().Play("MaskingLine");
+
+        StartCoroutine(MoveMask(mask, endSize, endAnch, amountOfTime));
+
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}s", minutes, seconds);
+    }
+
+    IEnumerator MoveMask(GameObject mask, Vector2 endSize, Vector2 endAnch, float timeToMove)
+    {
+        var currentSize = mask.GetComponent<RectTransform>().sizeDelta;
+        var currentPos = mask.GetComponent<RectTransform>().anchoredPosition;
+        var t = 0f;
+        while(t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            mask.GetComponent<RectTransform>().sizeDelta = Vector2.Lerp(currentSize, endSize, t);
+            mask.GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(currentPos, endAnch, t);
+            yield return null;
+        }
+    }
+}
