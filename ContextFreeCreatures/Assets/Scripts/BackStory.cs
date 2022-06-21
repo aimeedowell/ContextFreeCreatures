85e597505f2d95b81bcb6d66b6788cb581d59b89
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-06-21 17:21:58 +0100

    Finish Backstory and integrate into game

---
+++ ContextFreeCreatures/Assets/Scripts/BackStory.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackStory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("TutorialComplete") == 1)
+            SceneManager.LoadScene("LevelSelection");
+        else
+            StartCoroutine(MoveScenes());
+
+        
+    }
+
+    IEnumerator MoveScenes() 
+    {
+        yield return new WaitForSeconds(32f);
+        SceneManager.LoadScene("StartPage");
+    }
+
+}
