4b98df459e9245792111fa9439e5630d7489ef18
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-06-25 12:34:47 +0100

    Add Shop to each level

--- ContextFreeCreatures/Assets/Scripts/Shop.cs
+++ ContextFreeCreatures/Assets/Scripts/Shop.cs
@@ -7,24 +7,32 @@ public class Shop : MonoBehaviour
 {
     public Text coinScore;
     public GameObject shopMenu;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject firstRule;
+    public GameObject startNode;
 
     public void RestartBonBon()
     {
-        int newScore = StaticVariables.CoinCount - 100;
+        UpdateCoins(100);
+        shopMenu.SetActive(false);
+    }
+
+    public void StartNodeToffee()
+    {
+        if (startNode.activeSelf == true)
+        {
+            UpdateCoins(150);
+            shopMenu.SetActive(false);
+
+            this.gameObject.GetComponent<LevelController>().ReplaceNode(firstRule.GetComponent<RuleContents>().GetCreatureImage(), startNode.GetComponent<RectTransform>().anchoredPosition);
+            this.gameObject.GetComponent<LevelController>().GetContents(firstRule.GetComponent<RuleContents>().GetRuleImages(), startNode.gameObject, startNode.GetComponent<RectTransform>().transform.localPosition.y);
+            startNode.SetActive(false);
+        }
+    }
+
+    void UpdateCoins(int price)
+    {
+        int newScore = StaticVariables.CoinCount - price;
         StaticVariables.CoinCount = newScore;
         PlayerPrefs.SetInt("Coins", StaticVariables.CoinCount);
-
-        shopMenu.SetActive(false);
     }
 }
