8115808d24624a67e45cb3498dfc3dff54ddfdbf
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-06-29 16:17:50 +0100

    Add skill level to player profile

--- ContextFreeCreatures/Assets/Scripts/PlayerProfile.cs
+++ ContextFreeCreatures/Assets/Scripts/PlayerProfile.cs
@@ -6,10 +6,19 @@ using UnityEngine.UI;
 public class PlayerProfile : MonoBehaviour
 {
     public GameObject playerProfile;
+    public GameObject cup;
 
     public Slider slider;
     public Text noOfStars;
     public Text noOfBadges;
+
+    public Text skillLevel;
+    public Sprite silverCup;
+    public Sprite goldCup;
+
+    int maxLevel = 0;
+    int maxStars = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +31,7 @@ public class PlayerProfile : MonoBehaviour
         SetLevelSliderValue();
         SetStarValue();
         SetBadgeValue();
+        SetPlayerSkillLevel();
     }
 
     public void OnProfileExitClick()
@@ -34,6 +44,7 @@ public class PlayerProfile : MonoBehaviour
         //  Find max level... 
         int val = this.gameObject.GetComponent<LevelSelectionPage>().maxLevel;
         slider.value = val;
+        maxLevel = val;
     }
 
     void SetStarValue()
@@ -43,12 +54,39 @@ public class PlayerProfile : MonoBehaviour
                     StaticVariables.Level7Stars + StaticVariables.Level8Stars + StaticVariables.Level9Stars +
                     StaticVariables.Level10Stars;
         noOfStars.GetComponent<Text>().text = count.ToString();
+        maxStars = count;
     }
     void SetBadgeValue()
     {
         int count = StaticVariables.BadgeSymmetry + StaticVariables.BadgeEmptyWord + StaticVariables.BadgeSplit +
                     StaticVariables.BadgeTimer + StaticVariables.BadgeColourNodes;
         noOfBadges.GetComponent<Text>().text = count.ToString();
+    }
+
+    void SetPlayerSkillLevel()
+    {
+        string skill = "Newbie";
+        if ((maxLevel >= 0 && maxLevel < 3) && (maxStars >= 0 && maxStars < 6))
+            skill = "Newbie";
+        else if ((maxLevel >= 3 && maxLevel < 7) && (maxStars >= 6 && maxStars < 21))
+            skill = "Rookie";
+        else if ((maxLevel >= 7 && maxLevel < 10) && (maxStars >= 21 && maxStars < 25))
+        {
+            skill = "Intermediate";
+            cup.GetComponent<Image>().sprite = silverCup;
+        }
+        else if (maxLevel == 10 && (maxStars <= 25))
+        {
+            skill = "Expert";
+            GetComponent<Image>().sprite = goldCup;
+        }
+        else if (maxLevel == 10 && (maxStars >= 25 && maxStars <= 30))
+        {
+            skill = "Master";
+            GetComponent<Image>().sprite = goldCup;
+        }
+        skillLevel.GetComponent<Text>().text = skill; 
+
     }
 
 }
